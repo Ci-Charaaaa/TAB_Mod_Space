4b8b114ead5a4aafc18372ed0cc96983f502725c
吴丕博 <13942694716@163.com>
2026-03-28 21:37:38 +0800

    新建了项目，实体复制

---
+++ entity_copy/entity_copy/entity_copy.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DXVision;
+using ZX;
+using AX.ModLoader;
+using HarmonyLib;
+
+
+
+namespace entity_copy.entity_copy
+{
+    internal class entity_copy
+    {
+    }
+}
